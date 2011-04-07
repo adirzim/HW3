6acaf855d4c33ba19e9f35564c3ae318484d484d
Adir Zimerman <adirzim@gmail.com>
2011-04-07 22:05:10 +0300

    header edited

--- HW3/memPage_t.h
+++ HW3/memPage_t.h
@@ -1,36 +1,49 @@
 #ifndef MemPage_t_H_
 #endif MemPage_t_H_
 
+#include "memPool_t.h"
+
 class memPage_t{
 
+		//TODO: make memPage internal to memPool only
 public:
 
+	//Static Member
+	static int PageSize;								//Default Page Size
+
 	//CTOR & DTOR
-	memPage_t();										//default CTOR
+	memPage_t();										//default CTOR - from PageSize
+	memPage_t(int size);								//CTOR from user size
 	~memPage_t();										//DTOR
 
-	void setPosition(int newAddress);					//set current position in memory page
+	void setPosition(int newAddress);					//set current position in memory page (value between 0 to _size)
 	memPage_t* GetPosition() const;						//get current position in memory page
 	
 	//page status information
 	bool IsPageEmpty();									//is page empty
 	bool IsPageFull();									//is page Full
-	int actualSize();									//actual size of a page(how many bytes really written in page)
-	int Capacity();										//capacity of a page (length)
-
-	//TODO: add function								//read data from page
-	//TODO: add function								//write data to page
+	int GetActualSize();									//actual size of a page(how many bytes really written in page)
+	int GetCapacity();										//capacity of a page (length)
+	
+	template<class T>
+	T* read(int sizeOfT);								//read data from page
+	void write(const T &t, int sizeOfT);				//write data to page
 
 	memPage_t* NextPage();								//a pointer to the next page
 	memPage_t* PreviousPage();							//a pointer to the previous page
 	
-
 private:
 
 	//Disabling copy
 	memPage_t(const memPage_t &m);						//disable copy CTOR
 	const memPage_t &operator=(const memPage_t &m);		//disable assignment operator
 
+	//Page properties
+	int _size;											//how many bytes really written in page
+	int _capacity;										//capacity of a page in bytes
+	int position;										//current position in memory page
+
+	//TODO: choose STL to implement memPage
 
 
 };
--- HW3/memPool_t.h
+++ HW3/memPool_t.h
@@ -1,12 +1,11 @@
 #ifndef MemPool_t_H_ 
 #endif MemPool_t_H_
 
+#include "memPage_t.h"
+
 class memPool_t{
 
 public:
-
-	//Static Member
-	static int PageSize;
 
 	//CTOR & DTOR
 	memPool_t();										//default CTOR - 1 empty page
@@ -16,14 +15,15 @@ public:
 	//public methods
 	int GetCurrentPosition();							//get the current position in the memPool_t (how many bytes really written in memPool_t
 	void SetCurrentPosition(int newPosition);			//set the current position (between 0-PageSize)
-	//TODO: T& read(T &t, int sizeOfT);					//read data from manager
-	//TODO: void write(T &t, int sizeOfT);				//write data to manager
+	template<class T>
+	T& read(const T &t, int sizeOfT);					//read data from manager
+	void write(const T &t, int sizeOfT);				//write data to manager
 	void CreateMemPage();								//create memory page upon demand
 
 	//memPool information
 	bool IsEmpty();										//is memPool is Empty?
 	int GetActualSize();								//actual size of the object memPool_t
-	int GetCapacity();									//capacity of the manager
+	int GetCapacity();									//capacity of the manager (how many bytes are written in this pool)
 	int GetNumberOfPages();								//get the number of memory page
 	memPage_t* GetFirstMemPage();						//get pointer to the first memory page
 	memPage_t* GetLastMemPgae();						//get pointer to the last memory page
@@ -38,5 +38,7 @@ private:
 	//disable copy
 	memPool_t(const memPool_t &m);						//disable copy CTOR
 	const memPool_t &operator=(const memPool_t &m);		//disable assignment operator
+
+	//TODO: choose STL to store memPage
 
 };
